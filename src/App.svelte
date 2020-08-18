9b75be86162dacae2511adbd51d09ab958897c50
msopyan <faqihsopyan@gmail.com>
2020-08-18 18:56:32 +0700

    charitylist

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,18 @@
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<script>import CharityList from "./CharityList.svelte";
 
+  let title = "Charity";
+  setTimeout(function () {
+	  title = " Donation";
+  }, 2000);
+</script>
+
+<h1>{title}</h1>
+<CharityList />
+
+<style>
+	h1 {
+		size: 3em;
+		color: red;
+	}
+</style>
