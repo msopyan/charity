54b990006595585d000df65d9635e2f6b8c40284
msopyan <faqihsopyan@gmail.com>
2020-08-27 15:00:17 +0700

    update color

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-=======
-
 <script>import CharityList from "./CharityList.svelte";
->>>>>>> 9b75be86162dacae2511adbd51d09ab958897c50
-
   let title = "Charity";
   setTimeout(function () {
 	  title = " Donation";
